9be2c39485762d5a2041db1d362c815165fef1a1
James Rollings <43218980+jamesrollings@users.noreply.github.com>
2023-01-01 14:24:18 +0000

    use a treeview widget instead of a listbox
    plus other fixes, tweaks and improvements

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 # pylint: disable=cell-var-from-loop, attribute-defined-outside-init, line-too-long
 """ App to calculate shopping costs """
 import tkinter as tk
-from tkinter import messagebox, END, ACTIVE, filedialog, IntVar, StringVar
-from datetime import datetime
+from tkinter import ttk, messagebox, END, filedialog, IntVar, StringVar, CENTER
 from ast import literal_eval
 
 class Application(tk.Frame):
@@ -32,8 +31,8 @@ class Application(tk.Frame):
 
     def define_widgets(self):
         """ Creates the widgets """
-        self.list_box_frame = tk.Frame(self, width=80)
-        self.list_box = tk.Listbox(self.list_box_frame, width=80, selectmode='multiple')
+        self.tree_view_frame = tk.Frame(self, width=80)
+        self.list_box = tk.Listbox(self.tree_view_frame, width=80, selectmode='multiple')
         self.label_total = tk.Label(self, text='£{:,.2f}'.format(0.00), width=80, fg='lime green', font='Helvetica 9 bold')
         self.frame_entry = tk.Frame(self, width=80)
         self.label_product = tk.Label(self.frame_entry, width=10, text='Product:')
@@ -44,15 +43,16 @@ class Application(tk.Frame):
         self.radio_split = tk.Checkbutton(self.frame_entry, width=5, text='Split', onvalue=1, offvalue=0, variable=self.split_selected)
         self.button_add = tk.Button(self, textvariable=self.operation_text, default="active", command=self.add_entry)
         self.button_duplicate = tk.Button(self, text='Duplicate', command=self.duplicate_entry, state='disabled')
-        self.button_delete = tk.Button(self, text='Delete', command=self.delete_entry)
-        self.list_box_scroll_bar = tk.Scrollbar(self.list_box_frame)
+        self.button_delete = tk.Button(self, text='Delete', command=self.delete_entry, state='disabled')
+        self.button_import = tk.Button(self, text='Import', command=self.open_file)
         self.button_export = tk.Button(self, text='Export', command=self.save_as)
         self.button_subtract = tk.Checkbutton(self, width=6, text='Subtract?', onvalue=1, offvalue=0, variable=self.operation, command=self.set_operation_text)
+        self.tree_view = ttk.Treeview(self.tree_view_frame, column=("c1", "c2", "c3"), show='headings', height=5)
+        self.tree_view_scrollbar = ttk.Scrollbar(self.tree_view_frame, orient ="vertical", command = self.tree_view.yview)
 
     def place_widgets(self):
         """ Places the widgets on the GUI """
-        self.list_box_frame.grid(column=1, row=3)
-        self.list_box.pack(side=tk.LEFT, fill=tk.BOTH)
+        self.tree_view_frame.grid(column=1, row=3, pady=5)
         self.label_total.grid(column=1, row=4)
         self.frame_entry.grid(column=1, row=2)
         self.label_product.pack(side=tk.LEFT, fill=tk.BOTH)
@@ -64,11 +64,18 @@ class Application(tk.Frame):
         self.button_add.grid(column=2, row=2)
         self.button_duplicate.grid(column=2, row=3)
         self.button_delete.grid(column=2, row=4)
-        self.list_box_scroll_bar.pack(side=tk.RIGHT, fill=tk.BOTH)
-        self.list_box.config(yscrollcommand = self.list_box_scroll_bar.set)
-        self.list_box_scroll_bar.config(command = self.list_box.yview)
+        self.tree_view_scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH)
+        self.tree_view.config(yscrollcommand=self.tree_view_scrollbar.set)
+        self.button_import.grid(column=0, row=3)
         self.button_export.grid(column=0, row=4)
         self.button_subtract.grid(column=0, row=2)
+        self.tree_view.column("# 1", anchor=CENTER)
+        self.tree_view.heading("# 1", text="ItemId")
+        self.tree_view.column("# 2", anchor=CENTER)
+        self.tree_view.heading("# 2", text="Product")
+        self.tree_view.column("# 3", anchor=CENTER)
+        self.tree_view.heading("# 3", text="Cost")
+        self.tree_view.pack(side=tk.LEFT, fill=tk.BOTH)
 
     def set_keyboard_bindings(self):
         """ Sets keyboard bindings """
@@ -76,11 +83,14 @@ class Application(tk.Frame):
         self.master.bind('<Control-a>', self.toggle_select_box_highlighting)
         self.master.bind('<Delete>', self.delete_entry)
         self.master.bind('<Control-v>', self.paste_from_clipboard)
-        self.list_box.bind('<<ListboxSelect>>', self.on_select)
+        self.tree_view.bind('<<TreeviewSelect>>', self.on_select)
 
     def on_select(self, evt):
         """ handles selecting inside the listbox, enables the duplicate button if a row has been selected """
-        self.button_duplicate.config(state='normal' if len(evt.widget.curselection()) == 1 else 'disabled')
+        has_one_selected = len(evt.widget.selection()) == 1
+        has_at_least_one_selected = len(evt.widget.selection()) >= 1
+        self.button_delete.config(state='normal' if has_at_least_one_selected else 'disabled')
+        self.button_duplicate.config(state='normal' if has_one_selected else 'disabled')
 
     def set_operation_text(self):
         """ Sets the operation text for adding or subtracting the entered product value """
@@ -90,16 +100,31 @@ class Application(tk.Frame):
         else:
             self.operation_text.set('Add')
 
+    def get_next_id(self):
+        """ Gets the next id to use, otherwise deleted entries would result in gaps """
+        if len(self.items) == 0:
+            return 1
+
+        highest_current_value = sorted(self.items, key=lambda x: x[0], reverse=True)[0][0]
+        return highest_current_value + 1
+
+    def create_entry(self, product, cost):
+        """ Creates an entry """
+        self.item_id = self.get_next_id()
+        return (self.item_id, product, cost)
+
     def add_entry(self, event=None):
         """ Adds an entry to the list box """
         if event is None:
             del event
         product_name = self.entry_product.get()
         if product_name == '':
-            return messagebox.showerror('Error', 'Product must not be empty')
+            messagebox.showerror('Error', 'Product must not be empty')
+            return
         cost = self.entry_cost.get()
         if cost == '':
-            return messagebox.showerror('Error', 'Cost must not be empty')
+            messagebox.showerror('Error', 'Cost must not be empty')
+            return
         try:
             cost = float(self.entry_cost.get())
         except ValueError:
@@ -109,23 +134,9 @@ class Application(tk.Frame):
         vat_state = self.vat_amount.get() == 1
         split_state = self.split_selected.get() == 1
         calculated_list = self.calculate([product_name, cost, vat_state, split_state])
-        self.item_id = self.item_id + 1
-        record = {
-            'ItemID': self.item_id,
-            'Product': calculated_list['Product'],
-            'InitialCost': calculated_list['InitialCost'],
-            'VAT': calculated_list['VAT'],
-            'Split': calculated_list['Split'],
-            'FinalCost': calculated_list['FinalCost'],
-            'DateAdded': datetime.utcnow()
-        }
+        record = self.create_entry(product_name, calculated_list['FinalCost'])
         self.items.append(record)
-        list_box_entry = str({
-            'ItemID': record['ItemID'],
-            'Product': record['Product'],
-            'FinalCost': record['FinalCost']
-        })
-        self.list_box.insert(END, list_box_entry)
+        self.tree_view.insert("", 'end', values=record)
         self.entry_product.delete(0, END)
         self.entry_cost.delete(0, END)
         self.radio_split.deselect()
@@ -137,40 +148,26 @@ class Application(tk.Frame):
         """ Deletes an entry from the list box """
         if event is None:
             del event
-        if self.list_box.get(ACTIVE) == '':
+        selection = self.tree_view.selection()[0]
+        entry = self.tree_view.item(selection)
+        if entry['values'] == '':
             return
-        selected_text_list = [self.list_box.get(i) for i in self.list_box.curselection()]
-        if len(selected_text_list) > 1:
-            for i in selected_text_list:
-                self.list_box.delete(self.list_box.get(0, END).index(i))
-                self.update_total(float(literal_eval(i)['FinalCost']), '-')
-                self.items = list(filter(lambda x: x['ItemID'] != literal_eval(i)['ItemID'], self.items))
-            return
-        listbox_value = self.list_box.get(ACTIVE)
-        cost_to_remove = literal_eval(listbox_value)['FinalCost']
-        idx = self.list_box.get(0, END).index(listbox_value)
-        self.list_box.delete(idx)
-        self.update_total(cost_to_remove, '-')
+        self.tree_view.delete(selection)
+        self.items = list(filter(lambda x: x[0] != entry['values'][0], self.items))
+        self.update_total(float(entry['values'][2]), '-')
 
     def duplicate_entry(self):
         """ Duplicates an entry in the list box """
-        value = self.list_box.get(ACTIVE)
-        if value == '':
+        entry = self.tree_view.item(self.tree_view.focus())
+        if entry['values'] == '':
             return
-        original_item_id = literal_eval(value)['ItemID']
-        copied_entry = self.items[list(x['ItemID'] == original_item_id for x in self.items).index(True)]
-        copied_entry = copied_entry.copy()
-        self.item_id = self.item_id + 1
-        copied_entry['ItemID'] = self.item_id
-        copied_entry['DateAdded'] = datetime.utcnow()
-        list_box_string_value = str({
-            'ItemID': copied_entry['ItemID'],
-            'Product': copied_entry['Product'],
-            'FinalCost': copied_entry['FinalCost'],
-        })
+
+        product = entry['values'][1]
+        cost = entry['values'][2]
+        copied_entry = self.create_entry(product, cost)
         self.items.append(copied_entry)
-        self.list_box.insert(END, list_box_string_value)
-        self.update_total(copied_entry['FinalCost'], '+')
+        self.tree_view.insert("", 'end', values=copied_entry)
+        self.update_total(float(cost), '+')
 
     def update_total(self, value, operation):
         """ Updates the running total """
@@ -194,7 +191,7 @@ class Application(tk.Frame):
     def format_entry(entry):
         """ returns a string of a formatted entry """
         entry = literal_eval(entry)
-        return 'ItemID: {}, Product: {}, Cost: {}\n'.format(entry['ItemID'], entry['Product'], entry['FinalCost'])
+        return "'ItemID': '{}', 'Product': '{}', 'Cost': '{}'\n".format(entry['ItemID'], entry['Product'], entry['FinalCost'])
 
     def save_as(self):
         """ Opens a filedialog and saves current entries to disk """
@@ -204,12 +201,47 @@ class Application(tk.Frame):
                 return
 
             contents = list(self.list_box.get(0, END))
-            new_file_contents = ''
+            new_file_contents = 'Shopping Calculator\n'
             for entry in contents:
                 new_file_contents += self.format_entry(entry)
 
             new_file.write(new_file_contents)
             new_file.close()
+
+    def open_file(self):
+        """ Opens a filedialog to select a file """
+
+        existing_file = filedialog.askopenfilename(initialdir = "/",
+                                          title = "Select a File",
+                                          filetypes = (("Text files",
+                                                        "*.txt*"),
+                                                       ("all files",
+                                                        "*.*")))
+        contents = open(existing_file, 'r').read()
+
+        if contents is None or len(contents) == 0:
+            messagebox.showerror('Error', 'This file contains no data to import')
+            return
+
+        contents = contents.split('\n')
+
+        if contents[0] != 'Shopping Calculator':
+            messagebox.showerror('Error', 'This file cannot be imported')
+            return
+
+        del contents[0]
+
+        for line in contents:
+            if len(line) == 0:
+                continue
+
+            line = '{} {} {}'.format('{', line, '}')
+            object_line = literal_eval(line)
+            self.items.append(object_line)
+            self.list_box.insert(END, line.replace('"', '').replace('{', '').replace('}', ''))
+            row_operation = '-' if self.operation.get() == '1' else '+'
+            self.update_total(float(object_line['Cost']), row_operation)
+            self.entry_product.focus()
 
     def paste_from_clipboard(self, event=None):
         """ function to handle pasting into the list box """
